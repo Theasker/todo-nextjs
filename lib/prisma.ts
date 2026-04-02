28b9065883473bc0496e8c146a3ef7e5887c5bbc
Theasker <theasker@gmail.com>
2026-04-02 17:42:12 +0200

    arreglar lib/prisma/ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaLibSql } from '@prisma/adapter-libsql'
-import { createClient } from '@libsql/client'
 
 export function getPrisma() {
-  const libsql = createClient({
+  const adapter = new PrismaLibSql({
     url: process.env.DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN,
   })
-  const adapter = new PrismaLibSql(libsql)
   return new PrismaClient({ adapter })
 }
